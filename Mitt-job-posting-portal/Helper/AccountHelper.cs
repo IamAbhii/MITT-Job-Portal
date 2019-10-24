220d059777b91147c430f17c6b2d0e230eb4c0d1
Parth <parth280197@gmail.com>
2019-10-24 09:56:13 -0500

    Registration Page added for Instructor

---
+++ Mitt-job-posting-portal/Helper/AccountHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Mitt_job_posting_portal.Models;
+
+namespace Mitt_job_posting_portal.Helper
+{
+  public class AccountHelper
+  {
+    ApplicationDbContext db;
+    private RoleManager<IdentityRole> _roleManager;
+    private UserManager<User> _userManager;
+    public AccountHelper(ApplicationDbContext db)
+    {
+      this.db = db;
+      _roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+      _userManager = new UserManager<User>(new UserStore<User>(db));
+    }
+  }
+}
