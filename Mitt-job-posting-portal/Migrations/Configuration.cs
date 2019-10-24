89edc4f249b86d021f7b11b654e6c2faef80d517
Parth <parth280197@gmail.com>
2019-10-24 11:09:38 -0500

    Seeding added for Courses

--- Mitt-job-posting-portal/Migrations/Configuration.cs
+++ Mitt-job-posting-portal/Migrations/Configuration.cs
@@ -1,23 +1,27 @@
 namespace Mitt_job_posting_portal.Migrations
 {
-    using System;
-    using System.Data.Entity;
-    using System.Data.Entity.Migrations;
-    using System.Linq;
+  using Mitt_job_posting_portal.Models;
+  using System.Data.Entity.Migrations;
 
-    internal sealed class Configuration : DbMigrationsConfiguration<Mitt_job_posting_portal.Models.ApplicationDbContext>
+  internal sealed class Configuration : DbMigrationsConfiguration<Mitt_job_posting_portal.Models.ApplicationDbContext>
+  {
+
+    public Configuration()
     {
-        public Configuration()
-        {
-            AutomaticMigrationsEnabled = false;
-        }
-
-        protected override void Seed(Mitt_job_posting_portal.Models.ApplicationDbContext context)
-        {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data.
-        }
+      AutomaticMigrationsEnabled = false;
     }
+
+    protected override void Seed(Mitt_job_posting_portal.Models.ApplicationDbContext context)
+    {
+      context.Course.AddOrUpdate(c => c.Id,
+        new Course() { Name = "Software Developer" },
+        new Course() { Name = "Networking" },
+        new Course() { Name = "Early Childhood Education" },
+        new Course() { Name = "Culinary" });
+      //  This method will be called after migrating to the latest version.
+
+      //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+      //  to avoid creating duplicate seed data.
+    }
+  }
 }
