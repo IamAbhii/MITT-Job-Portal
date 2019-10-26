77cf5ae862c2ea4c2819ae3e61dcef50b493135e
Parth <parth280197@gmail.com>
2019-10-26 14:24:18 -0500

    Seeding method updated with Seeding helper implementation

---
+++ Mitt-job-posting-portal/Helper/SeedingHelper.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Mitt_job_posting_portal.Models;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+
+namespace Mitt_job_posting_portal.Helper
+{
+  public class SeedingHelper
+  {
+    UserManager<User> userManager;
+    RoleManager<IdentityRole> roleManager;
+    ApplicationDbContext context;
+    public SeedingHelper(ApplicationDbContext context)
+    {
+      this.context = context;
+      userManager = new UserManager<User>(new UserStore<User>(context));
+      roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+    }
+    public void SeedCourses(string[] courses)
+    {
+      foreach (string course in courses)
+      {
+        context.Course.AddOrUpdate(c => c.Id, new Course() { Name = course });
+      }
+    }
+    public void SeedRoles(string[] roles)
+    {
+      foreach (string role in roles)
+      {
+        if (!roleManager.RoleExists(role))
+        {
+          var roleresult = roleManager.Create(new IdentityRole(role));
+        }
+      }
+    }
+    public void SeedAdmin(string name)
+    {
+      string userName = name.ToLower() + "@ip.com";
+      string password = "Password-1";
+      User user = userManager.FindByName(userName);
+      if (user == null)
+      {
+        user = new User()
+        {
+          UserName = userName,
+          Email = userName,
+          EmailConfirmed = true
+        };
+        IdentityResult userResult = userManager.Create(user, password);
+        if (userResult.Succeeded)
+        {
+          var result = userManager.AddToRole(user.Id, "Admin");
+          context.Admin.AddOrUpdate(new Admin() { Name = name, User = user });
+        }
+      }
+    }
+    public void SeedInstructor(string name, string courseName)
+    {
+      string userName = name.ToLower() + "@ip.com";
+      string password = "Password-1";
+      var user = userManager.FindByName(userName);
+      if (user == null)
+      {
+        user = new User()
+        {
+          UserName = userName,
+          Email = userName,
+          EmailConfirmed = true
+        };
+        IdentityResult userResult = userManager.Create(user, password);
+        if (userResult.Succeeded)
+        {
+          var result = userManager.AddToRole(user.Id, "Instructor");
+          context.Instructor.AddOrUpdate(new Instructor()
+          {
+            Name = name,
+            Courses = new List<CourseInstructor>()
+              {
+                new CourseInstructor()
+                {
+                  Course = context.Course.Where(c=>c.Name==courseName).FirstOrDefault(), InstructorId = user.Id
+                }
+              },
+            User = user
+          });
+        }
+      }
+    }
+    public void SeedStudent(string name, string courseName)
+    {
+      string userName = name.ToLower() + "@ip.com";
+      string password = "Password-1";
+      var user = userManager.FindByName(userName);
+      if (user == null)
+      {
+        user = new User()
+        {
+          UserName = userName,
+          Email = userName,
+          EmailConfirmed = true
+        };
+        IdentityResult userResult = userManager.Create(user, password);
+        if (userResult.Succeeded)
+        {
+          var result = userManager.AddToRole(user.Id, "Student");
+          context.Student.AddOrUpdate(new Student()
+          {
+            Name = name,
+            BirthDate = new System.DateTime(1997, 01, 28),
+            PreviousEducation = "N/A",
+            PreviousEducationDetail = "N/A",
+            CourseId = context.Course.Where(c => c.Name == courseName).First().Id,
+            User = user
+          });
+        }
+      }
+    }
+    public void SeedEmployer(string companyName)
+    {
+      string userName = companyName.ToLower() + "@ip.com";
+      string password = "Password-1";
+      var user = userManager.FindByName(userName);
+      if (user == null)
+      {
+        user = new User()
+        {
+          UserName = userName,
+          Email = userName,
+          EmailConfirmed = true
+        };
+        IdentityResult userResult = userManager.Create(user, password);
+        if (userResult.Succeeded)
+        {
+          var result = userManager.AddToRole(user.Id, "Employer");
+          context.Employer.AddOrUpdate(new Employer()
+          {
+            CompanyName = companyName,
+            CompanyDetails = "N/A",
+            Companylinks = "N/A",
+            EmailAdress = "N/A",
+            User = user
+          });
+        }
+      }
+    }
+
+  }
+}
--- Mitt-job-posting-portal/Migrations/Configuration.cs
+++ Mitt-job-posting-portal/Migrations/Configuration.cs
@@ -1,11 +1,13 @@
 namespace Mitt_job_posting_portal.Migrations
 {
+  using Microsoft.AspNet.Identity;
+  using Microsoft.AspNet.Identity.EntityFramework;
+  using Mitt_job_posting_portal.Helper;
   using Mitt_job_posting_portal.Models;
   using System.Data.Entity.Migrations;
 
   internal sealed class Configuration : DbMigrationsConfiguration<Mitt_job_posting_portal.Models.ApplicationDbContext>
   {
-
     public Configuration()
     {
       AutomaticMigrationsEnabled = false;
@@ -13,15 +15,42 @@ namespace Mitt_job_posting_portal.Migrations
 
     protected override void Seed(Mitt_job_posting_portal.Models.ApplicationDbContext context)
     {
-      context.Course.AddOrUpdate(c => c.Id,
-        new Course() { Name = "Software Developer" },
-        new Course() { Name = "Networking" },
-        new Course() { Name = "Early Childhood Education" },
-        new Course() { Name = "Culinary" });
-      //  This method will be called after migrating to the latest version.
+      SeedingHelper seedingHelper = new SeedingHelper(context);
+      var userManager = new UserManager<User>(new UserStore<User>(context));
+      var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
 
-      //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-      //  to avoid creating duplicate seed data.
+      //Add Courses
+      var courses = new string[] { "Software Developer", "Networking", "Early Childhood Education", "Culinary" };
+      seedingHelper.SeedCourses(courses);
+
+      //Add roles
+      var roles = new string[] { "Admin", "Instructor", "Student", "Employer" };
+      seedingHelper.SeedRoles(roles);
+
+      //add Admin
+      seedingHelper.SeedAdmin("Admin");
+
+      //add Instructor
+      seedingHelper.SeedInstructor("Instructor1", "Software Developer");
+      seedingHelper.SeedInstructor("Instructor2", "Networking");
+
+      //add Students
+      seedingHelper.SeedStudent("SDStudent1", "Software Developer");
+      seedingHelper.SeedStudent("SDStudent2", "Software Developer");
+      seedingHelper.SeedStudent("SDStudent3", "Software Developer");
+      seedingHelper.SeedStudent("SDStudent4", "Software Developer");
+      seedingHelper.SeedStudent("NStudent1", "Networking");
+      seedingHelper.SeedStudent("NStudent2", "Networking");
+      seedingHelper.SeedStudent("NStudent3", "Networking");
+      seedingHelper.SeedStudent("NStudent4", "Networking");
+
+      //add Employer
+      seedingHelper.SeedEmployer("Infosys");
+      seedingHelper.SeedEmployer("MITT");
+      seedingHelper.SeedEmployer("Ibex");
+      seedingHelper.SeedEmployer("COFW");
+      seedingHelper.SeedEmployer("IndianTech");
+
     }
   }
 }
